579d1e0e39ab19a41096d3d74c94ce3137a940b6
Ayman Hazem <aymooongsr2@gmail.com>
2025-04-20 08:04:41 +0200

    Init Commit

---
+++ src/main/java/com/ayman/taskracker/TaskrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ayman.taskracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayman/taskracker/TaskrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayman.taskracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
